cea46a277f6bafd1230fa68516e4c931e9c975b6
gray1121 <gerui1121@hotmail.com>
2016-08-26 15:09:22 -0400

    first

--- Rick/Median of Two Sorted Arrays.java
+++ Rick/Median of Two Sorted Arrays.java
@@ -1,3 +1,9 @@
+/* O(n)的解法比较直观，直接merge两个数组，然后求中间值。
+而对于O(log(m+n))显然是用二分搜索了,
+相当于“Kth element in 2 sorted array”的变形。
+如果(m+n)为奇数，那么找到“(m+n)/2+1 th element in 2 sorted array”即可。
+如果（m+n）为偶数，需要找到(m+n)/2 th 及(m+n)/2+1 th，然后求平均。
+*/
 public class Solution {
     private int findKthSmallest(int[] a, int m, int begin1, int[] b, int n, int begin2, int k){
         if(m > n)   return findKthSmallest(b, n, begin2, a, m, begin1, k);
@@ -14,7 +20,7 @@ public class Solution {
     }
     public double findMedianSortedArrays(int[] nums1, int[] nums2){
         int m = nums1.length, n = nums2.length, sum = n + m;
-        if(sum % 2 != 0){
+        if(sum % 2 != 0){ // odd or even length of array
             return findKthSmallest(nums1, m, 0, nums2, n, 0, sum/2+1);
         }else{
             return (
