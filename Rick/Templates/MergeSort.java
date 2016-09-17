34dfa239231da6b6963cb04f4f1e52d1e6ec5483
gray1121 <gerui1121@hotmail.com>
2016-09-16 21:56:10 -0400

    typo

--- Rick/Templates/MergeSort.java
+++ Rick/Templates/MergeSort.java
@@ -1,14 +1,14 @@
 public class Solution {
     /**
-     * @param Aan integer array
+     * @param A is an integer array
      * @return void
      */
      public void sortIntegers2(int[] A){
-         //usea shared temp array, the extra memory is O(n) at least
+         //use a shared temp array, the extra memory is O(n) at least
          int[] temp = new int[A.length];
          mergeSort(A, 0, A.length - 1, temp);
      }
-     private void mergeSort(int[] A, int start,int end, int[] temp){
+     private void mergeSort(int[] A, int start, int end, int[] temp){
          if (start >= end){
              return;
          }
@@ -16,7 +16,7 @@ public class Solution {
          int mid = (start + end) / 2;
          
          mergeSort(A, start, mid, temp);
-         mergeSort(A, mid+1, end, temdp);
+         mergeSort(A, mid+1, end, temp);
          merge(A, start, mid, end, temp);
      }
      private void merge(int[] A, int start, int mid, int end, int[] temp){
@@ -24,7 +24,7 @@ public class Solution {
          int right = mid + 1;
          int index = start;
         // merge two sorted subarrays in A to temp array
-        while (left <= md && right <= end){
+        while (left <= mid && right <= end){
             if(A[left] < A[right]){
                 temp[++index] = A[++left];
             } else {
