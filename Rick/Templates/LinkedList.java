4b672abce756f5c87425f7b7a350a82b8b072d0f
gray1121 <gerui1121@hotmail.com>
2016-09-17 20:09:27 -0400

    Linkedlist

---
+++ Rick/Templates/LinkedList.java
@@ -0,0 +1,18 @@
+/**
+    Definition for singly-linked list
+    class ListNode {
+        int val;
+        ListNode next;
+        ListNode(int x){
+            val = x;
+            next = null;
+        }
+    }
+
+    Double-linked list
+    class DLinkedNode {
+        int key;
+        int value;
+        DLinkedNode pre;
+        DlinkedNode post;
+    }
