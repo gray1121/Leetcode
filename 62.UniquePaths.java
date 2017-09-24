37cc61b822657f5bd10dd86fa125dcbdaa357b08
gray1121 <gerui1121@hotmail.com>
2017-09-24 16:34:33 -0400

    coding style

--- 62.UniquePaths.java
+++ 62.UniquePaths.java
@@ -23,12 +23,12 @@ class Solution {
 // silimiar idea can be used to coin change
 public class Solution {
     public int uniquePaths(int m, int n) {
-        if(m <= 0 || n <= 0) 
+        if (m <= 0 || n <= 0) 
             return 0;
         int[] dp = new int[n];
         dp[0] = 1;
-        for(int i = 0; i < m; ++i){
-            for(int j = 1; j < n; ++j){
+        for (int i = 0; i < m; ++i) {
+            for (int j = 1; j < n; ++j) {
                 dp[j] += dp[j - 1];
             }
         }
